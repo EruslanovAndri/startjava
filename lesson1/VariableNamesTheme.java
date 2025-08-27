257804bce75b83cb24feeb1dfb30e06253025f32
EruslanovAndrei <eruslanov84as@gmail.com>
2025-08-27 20:11:04 +0300

    add new file VariableNamesTheme home work

---
+++ lesson1/VariableNamesTheme.java
@@ -0,0 +1,152 @@
+public class VariableNamesTheme{
+	public static void main(String[] args){
+
+		// Разные переменные.
+
+		System.out.println("Блок номер 1 \nРазные переменные");
+        
+        // Цифра
+
+		int didgit = 1;
+		System.out.println("Цифра: " + didgit);
+
+		// Сумма чисел 
+
+		int num1 = 5;
+		int num2 = 4;
+		int resultSum = num1 + num2;
+		System.out.println("Сумма чисел: " + resultSum);
+
+		// Произведение цифр
+
+		int multiplicationResult = num1 * num2;
+		System.out.println("Произведение цифр: " + multiplicationResult);
+
+		// Максимальное число
+
+		int maxNumber = 45;
+		System.out.println("Максимальное число: " + maxNumber);
+
+		// Kоличество десятков
+
+		int tensCount = 10;
+		System.out.println("Kоличество десятков: " + tensCount);
+
+		// вес собаки
+
+		int dogWeight = 15;
+		System.out.println("Вес собаки: " + dogWeight);
+
+		// исходное число
+
+		int originalNumber = 0;
+		System.out.println("Исходное число: " + originalNumber);
+
+		// процент по вкладу
+
+		int intersOnDeposit = 10;
+		System.out.println("Процент по вкладу: " + intersOnDeposit);
+
+		// символ &
+
+		char symbol = '&';
+		System.out.println("Символ: " + symbol);
+
+		// код ошибки
+
+		int errorCode = 404;
+		System.out.println("Код ошибки: " + errorCode);
+
+		// тип сообщения
+
+		String messageType = "NullPointerException";
+		System.out.println("Тип ошибки: " + messageType);
+
+		// число нулей
+
+		int numberOfZeros = 5;
+		System.out.println("Число нулей: " + numberOfZeros);
+
+		// уникальное число
+
+		int unigueNumber = 33;
+		System.out.println("Уникальное число: " + unigueNumber);
+
+		// случайное число
+
+		int rndNumber = 88;
+		System.out.println("случайное число: " + rndNumber);
+
+		// математическое выражение
+
+		String mathExpretion = "2 + 3";
+		System.out.println("Mатематическое выражение: " + mathExpretion);
+
+		// счет в игре
+
+		int scoreInGame = 23;
+		System.out.println("счет в игре: " + scoreInGame);
+
+		// максимальная длина
+
+		int maxLength = 15;
+		System.out.println("максимальная длина: " + maxLength);
+
+		// пункт меню
+
+		int menuItem = 2;
+		System.out.println("пункт меню: " + menuItem);
+
+		// стоимость кофе
+
+		int cofeePrice = 250;
+		System.out.println("стоимость кофе: " + cofeePrice);
+
+		// начальная дата
+
+		int startDate = 1;
+		System.out.println("начальная дата: " + startDate);
+
+     	// окончание диапазона
+
+     	int endOfRange = 40;
+     	System.out.println("Окончание диапазона: " + endOfRange);
+
+		// имя работника месяца
+
+		String employeeOfTheMonthName = "Max";
+		System.out.println("имя работника месяца: " + employeeOfTheMonthName);
+
+		// название электронной книги
+
+		String nameOfeBook = "Java";
+		System.out.println("название электронной книги: " + nameOfeBook);
+
+		// размер
+
+		int size = 5;
+		System.out.println("Размер: " + size);
+
+		// вместимость
+
+		int capacity = 128;
+		System.out.println("Вместимость: " + capacity);
+
+		// счетчик
+
+		int counter = 0;
+		System.out.println("Счетчик: " + counter);
+
+		// путь до файла
+
+		String pathToFile = "url.....";
+		System.out.println("Путь до файла: " + pathToFile);
+
+		// количество чисел в строке
+
+		int NumberOfNumbersInOneRow = 2;
+		System.out.println("количество чисел в строке: " + NumberOfNumbersInOneRow);
+		
+
+	}
+}
