f6be7a444e73b200237e3a47f5849936bc148632
EruslanovAndrei <eruslanov84as@gmail.com>
2025-09-21 15:48:39 +0300

    GuesNumber was checked

--- lesson1/GuesNumber.java
+++ lesson1/GuesNumber.java
@@ -1,42 +1,29 @@
 import java.util.Random;
 
 public class GuesNumber {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Random rnd = new Random();
 
-		Random rnd = new Random();
+        int startRange = 1;
 
-		int startRange = 1;
+        int endRange = 101;
 
-		int endRange = 101;
+        int hiddenNumber = rnd.nextInt(startRange, endRange);
 
-	    int	hiddenNumber = rnd.nextInt(startRange,endRange);
+        int myNumber = rnd.nextInt(startRange, endRange);
 
-		int myNumber = rnd.nextInt(startRange,endRange);
-
-
-		while (myNumber != hiddenNumber) {
-
-			if (myNumber < hiddenNumber) {
-
-				System.out.println(myNumber + " меньше того что загадал компьютер");
-
-				myNumber = rnd.nextInt((startRange + endRange) / 2,endRange);
-
-			} else if (myNumber > hiddenNumber) {
-
-				System.out.println(myNumber + " больше того что загадал компьютер");
-
-				myNumber = rnd.nextInt(startRange,(startRange + endRange) / 2);
-			
-			}
-		}
-
-
-		System.out.printf("%s %n %s %,d %n %s %,d "
-			,"Вы победили!"
-			,"Hidden number was:",hiddenNumber
-			,"Your number is:",myNumber);
-
-		
-	}
+        while (myNumber != hiddenNumber) {
+            if (myNumber < hiddenNumber) {
+                System.out.println(myNumber + " меньше того что загадал компьютер");
+                myNumber = rnd.nextInt((startRange + endRange) / 2, endRange);
+            } else if (myNumber > hiddenNumber) {
+                System.out.println(myNumber + " больше того что загадал компьютер");
+                myNumber = rnd.nextInt(startRange, (startRange + endRange) / 2);
+            }
+        }
+        System.out.printf("%s %n %s %,d %n %s %,d",
+                "Вы победили!",
+                "Hidden number was:", hiddenNumber,
+                "Your number is:", myNumber);
+    }
 }
