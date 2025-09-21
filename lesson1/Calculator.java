0ead5d7db57e7100a3cecce5238a71267d7f8322
EruslanovAndrei <eruslanov84as@gmail.com>
2025-09-21 14:28:04 +0300

    Проверил фаил Calculator.java на отсутствие ошибок при написание кода

--- lesson1/Calculator.java
+++ lesson1/Calculator.java
@@ -1,110 +1,67 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.lang.ArithmeticException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Calculator {
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-		System.out.println("Введите пример: ");
+        System.out.println("Введите пример: ");
 
-		String inputString = input.nextLine().replaceAll("\\s","");
+        String inputString = input.nextLine().replaceAll("\\s", "");
 
-		System.out.println("Введенный пример = "+ inputString);
+        System.out.println("Введенный пример = " + inputString);
 
-		int operatorIndex = -1; 
+        int operatorIndex = -1; 
 
+        if (inputString.contains("+")) {
+            operatorIndex = inputString.indexOf("+");
+        } else if (inputString.contains("-")) {
+            operatorIndex = inputString.indexOf("-");
+        } else if (inputString.contains("*")) {
+            operatorIndex = inputString.indexOf("*");
+        } else if (inputString.contains("/")) {
+            operatorIndex = inputString.indexOf("/");
+        } else if (inputString.contains("%")) {
+            operatorIndex = inputString.indexOf("%");
+        } else if (inputString.contains("^")) {
+            operatorIndex = inputString.indexOf("^");
+        }
 
-		if (inputString.contains("+")) {
+        String leftOperandStr = inputString.substring(0, operatorIndex);
 
-			operatorIndex = inputString.indexOf("+");
+        String rightOperandStr = inputString.substring(operatorIndex + 1, inputString.length());
 
-		} else if (inputString.contains("-")) {
+        Double leftOperand = Double.parseDouble(leftOperandStr);
 
-			operatorIndex = inputString.indexOf("-");
-			
-		} else if (inputString.contains("*")) {
+        Double rightOperand = Double.parseDouble(rightOperandStr);
 
-			operatorIndex = inputString.indexOf("*");
-			
-		} else if (inputString.contains("/")) {
+        char operator = inputString.charAt(operatorIndex);
 
-			operatorIndex = inputString.indexOf("/");
-			
-		} else if (inputString.contains("%")) {
+        Double result = 0d;
 
-			operatorIndex = inputString.indexOf("%");
-			
-		} else if (inputString.contains("^")) {
-
-			operatorIndex = inputString.indexOf("^");
-			
-		}	
-
-		String leftOperandStr = inputString.substring(0,operatorIndex);
-
-		String rightOperandStr = inputString.substring(operatorIndex + 1,inputString.length());
-
-		System.out.println("Левое число str = " + leftOperandStr + "\nПравое число str = " + rightOperandStr);
-
-		Double leftOperand = Double.parseDouble(leftOperandStr);
-
-		Double rightOperand = Double.parseDouble(rightOperandStr);
-
-		System.out.println("Левое число double = " + leftOperand + "\nПравое число double = " + rightOperand);
-
-		char operator = inputString.charAt(operatorIndex);
-
-		Double result = 0d;
-
-		switch (operator) {
-
-			case '+': result = leftOperand + rightOperand;
-				break;
-
-			case '-': result = leftOperand - rightOperand;
-				break;
-
-			case '*': result = leftOperand * rightOperand;
-				break;
-
-			case '/': if (rightOperand == 0) {
-
-					throw new ArithmeticException("Деление на ноль");
-
-				} else {
-
-					result = leftOperand / rightOperand;
-
-				}
-
-				break;
-
-			case '%': result = leftOperand % rightOperand;
-
-				break;
-
-			case '^': result = leftOperand;
-
-					
-				for (int i = 1; i < rightOperand; i++) {
-
-					result *= leftOperand;
-
-				}
-				break;
-
-		}
-
-		System.out.printf("%.2f %s %.2f %s %.2f", leftOperand,operator,rightOperand,"=",result);
-
-
-
-
-
-
-	}
+        switch (operator) {
+            case '+': result = leftOperand + rightOperand;
+                break;
+            case '-':result = leftOperand - rightOperand;
+                break;
+            case '*': result = leftOperand * rightOperand;
+                break;
+            case '/': if (rightOperand == 0) {
+                    throw new ArithmeticException("Деление на ноль");
+                } else {
+                    result = leftOperand / rightOperand;
+                }
+                break;
+            case '%': result = leftOperand % rightOperand;
+                break;
+            case '^': result = leftOperand;
+                for (int i = 1; i < rightOperand; i++) {
+                    result *= leftOperand;
+                }
+                break;
+            default: System.out.println("Yes");;
+        }
+        System.out.printf("%.2f %s %.2f %s %.2f", leftOperand, operator, rightOperand, "=", result);
+    }
 }
