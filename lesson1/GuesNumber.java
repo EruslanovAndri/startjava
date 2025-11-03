a7c1e7443a1628a6f85cf23cd891f27f676b900a
EruslanovAndrei <eruslanov84as@gmail.com>
2025-11-03 18:02:01 +0300

    GuesNumber изменил имя переменной myNumber на player и удалил пустые строки между переменными

--- lesson1/GuesNumber.java
+++ lesson1/GuesNumber.java
@@ -3,27 +3,23 @@ import java.util.Random;
 public class GuesNumber {
     public static void main(String[] args) {
         Random rnd = new Random();
-
         int startRange = 1;
-
         int endRange = 101;
-
         int hiddenNumber = rnd.nextInt(startRange, endRange);
+        int player = rnd.nextInt(startRange, endRange);
 
-        int myNumber = rnd.nextInt(startRange, endRange);
-
-        while (myNumber != hiddenNumber) {
-            if (myNumber < hiddenNumber) {
-                System.out.println(myNumber + " меньше того что загадал компьютер");
-                myNumber = rnd.nextInt((startRange + endRange) / 2, endRange);
-            } else if (myNumber > hiddenNumber) {
-                System.out.println(myNumber + " больше того что загадал компьютер");
-                myNumber = rnd.nextInt(startRange, (startRange + endRange) / 2);
+        while (player != hiddenNumber) {
+            if (player < hiddenNumber) {
+                System.out.println(player + " меньше того что загадал компьютер");
+                player = rnd.nextInt((startRange + endRange) / 2, endRange);
+            } else if (player > hiddenNumber) {
+                System.out.println(player + " больше того что загадал компьютер");
+                player = rnd.nextInt(startRange, (startRange + endRange) / 2);
             }
         }
         System.out.printf("%s %n %s %,d %n %s %,d",
                 "Вы победили!",
                 "Hidden number was:", hiddenNumber,
-                "Your number is:", myNumber);
+                "Your number is:", player);
     }
 }
