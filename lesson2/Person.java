c0cf31177112459f2e8bb0a587eb8221668c70fd
EruslanovAndrei <eruslanov84as@gmail.com>
2025-11-22 12:50:28 +0300

    add folder lesson2 with class Person.java

---
+++ lesson2/Person.java
@@ -0,0 +1,27 @@
+class Person {
+    String gender = "Male";
+    String name = "Andrei";
+    int height = 172;
+    int weight = 72;
+    int age = 40;
+    
+    void go() {
+        System.out.println("Метод go");
+    }
+
+    void sit() {
+        System.out.println("Метод sit");
+    }
+
+    void run() {
+        System.out.println("Метод run");
+    }
+
+    void speak() {
+        System.out.println("Метод speak");
+    }
+
+    void learnJava() {
+        System.out.println("Метод learn Java");
+    }
+}
