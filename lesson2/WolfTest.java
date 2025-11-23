12cc370dbdd7b304e7096bbb2d4a752ff7cc3eea
EruslanovAndrei <eruslanov84as@gmail.com>
2025-11-23 13:59:46 +0300

    add WolfTest class

---
+++ lesson2/WolfTest.java
@@ -0,0 +1,25 @@
+public class WolfTest {
+    public static void main(String[] args) {
+        Wolf wolfOne = new Wolf();
+        wolfOne.setGender("Male");
+        wolfOne.setNickname("lex");
+        wolfOne.setWeight(45);
+        wolfOne.setAge(7);
+        wolfOne.setFurColor("Grey");
+
+        System.out.printf("""
+            Gender = %s
+            Nickname = %s
+            Weight = %d
+            Age = %d
+            Fur color = %s
+                """, wolfOne.getGender(), wolfOne.getNickname(),
+                wolfOne.getWeight(), wolfOne.getAge(), wolfOne.getFurColor());
+
+        wolfOne.go();
+        wolfOne.sit();
+        wolfOne.run();
+        wolfOne.howl();
+        wolfOne.hunt();
+    }
+}
