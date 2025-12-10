155e335ec3c914a303e839de383f3ec619f2dbbc
EruslanovAndrei <eruslanov84as@gmail.com>
2025-12-10 15:09:15 +0300

    Static method

--- lesson2/Methods.java
+++ lesson2/Methods.java
@@ -2,7 +2,6 @@ import java.lang.reflect.Method;
 
 public class Methods {
     public static String getMethodName() {
-        String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-        return methodName;
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 }
