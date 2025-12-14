209e8222577f65ff56a769104edab64d74b5b69e
EruslanovAndrei <eruslanov84as@gmail.com>
2025-12-14 12:01:38 +0300

    изменил имена при инициализации объектов на более понятные, основываясь на именах роботов

--- lesson2/JaegerTest.java
+++ lesson2/JaegerTest.java
@@ -1,20 +1,20 @@
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger jaegerOne = new Jaeger();
-        jaegerOne.setModelName("Gipsy Danger");
-        jaegerOne.setMark("Mark-3");
-        jaegerOne.setOrigin("United States of America");
-        jaegerOne.setWeight(1.980f);
-        jaegerOne.setHeight(79.25f);
-        jaegerOne.setStrength(8);
-        jaegerOne.setArmor(6);
-        jaegerOne.setSpeed(7);
-        System.out.println(jaegerOne.toString());
-        jaegerOne.useRetractileSword();
-        jaegerOne.move();
-        Jaeger jaegerTwo = new Jaeger("Striker Eureka", "Mark-5", "Australia", 1.850f, 76.2f, 10, 9, 10);
-        System.out.println(jaegerTwo.toString());
-        jaegerTwo.useRetractileSword();
-        jaegerTwo.move();
+        Jaeger gipsyDanger = new Jaeger();
+        gipsyDanger.setModelName("Gipsy Danger");
+        gipsyDanger.setMark("Mark-3");
+        gipsyDanger.setOrigin("United States of America");
+        gipsyDanger.setWeight(1.980f);
+        gipsyDanger.setHeight(79.25f);
+        gipsyDanger.setStrength(8);
+        gipsyDanger.setArmor(6);
+        gipsyDanger.setSpeed(7);
+        System.out.println(gipsyDanger.toString());
+        gipsyDanger.useRetractileSword();
+        gipsyDanger.move();
+        Jaeger strikerEureka = new Jaeger("Striker Eureka", "Mark-5", "Australia", 1.850f, 76.2f, 10, 9, 10);
+        System.out.println(strikerEureka.toString());
+        strikerEureka.useRetractileSword();
+        strikerEureka.move();
     }
 }
