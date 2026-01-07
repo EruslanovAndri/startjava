65d48a41047f38e0792bb8f9c159117612f6c158
EruslanovAndrei <eruslanov84as@gmail.com>
2026-01-07 16:04:49 +0300

    BankTransactionsReverser class fixed two methods

--- src/com/startjava/lesson_2_3_4/array/BankTransactionsReverser.java
+++ src/com/startjava/lesson_2_3_4/array/BankTransactionsReverser.java
@@ -8,43 +8,38 @@ public class BankTransactionsReverser {
         printTransactions(transactions1);
         reverseTransactions(transactions1);
         printTransactions(null);
-        reverseTransactions(null);
-        int[] transactions2 = new int[] {5};
-        printTransactions(transactions2);
-        reverseTransactions(transactions2);
-        int[] transactions3 = new int[] {6, 8, 9, 1};
+        System.out.println(" В обратном порядке: " + Arrays.toString(reverseTransactions(null)));
+        int[] transactions3 = new int[]{5};
         printTransactions(transactions3);
-        reverseTransactions(transactions3);
-        int[] transactions4 = new int[] {13, 8, 5, 3, 2, 1, 1};
+        System.out.println(" В обратном порядке: " + Arrays.toString(reverseTransactions(transactions3)));
+        int[] transactions4 = new int[]{6, 8, 9, 1};
         printTransactions(transactions4);
-        reverseTransactions(transactions4);
+        System.out.println(" В обратном порядке: " + Arrays.toString(reverseTransactions(transactions4)));
+        int[] transactions5 = new int[]{13, 8, 5, 3, 2, 1, 1};
+        printTransactions(transactions5);
+        System.out.println(" В обратном порядке: " + Arrays.toString(reverseTransactions(transactions5)));
     }
 
     private static int[] reverseTransactions(int[] transactions) {
-        int[] reversedTransactions = null;
-        if (transactions == null) {
-            System.out.print("");
-        } else {
-            int i = transactions.length - 1;
-            reversedTransactions = new int[transactions.length];
-            for (int transaction : transactions) {
-                reversedTransactions[i] = transaction;
-                i--;
-            }
+      if (transactions != null) {
+          if (transactions.length != 0) {
+              System.out.print("");
+          } else {
+              System.out.println("массив нулевой длины");
+          }
+      } else {
+          return null;
+      }
+        int i = transactions.length;
+        int[] reversedTransactions = new int[transactions.length];
+        for (int transaction : transactions) {
+            --i;
+            reversedTransactions[i] = transaction;
         }
-
         return reversedTransactions;
     }
 
-    private static void printTransactions(int[] transactions) {
-        int[] reverseTransactions = reverseTransactions(transactions);
-        if (transactions == null) {
-            System.out.println("null (ошибка в данных)");
-        } else if (transactions.length == 0) {
-            System.out.println("Массив нулевой длины");
-        } else {
+    private static void printTransactions ( int[] transactions){
             System.out.println("Исходные транзакции: " + Arrays.toString(transactions));
-            System.out.println(" В обратном порядке: " + Arrays.toString(reverseTransactions));
-        }
     }
 }
