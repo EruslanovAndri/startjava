05929b90eb6a93b92065ef366c83c9c4eb951ad6
EruslanovAndrei <eruslanov84as@gmail.com>
2026-01-18 12:05:37 +0300

    add LIMIT_NUMBER as a constant, rename method printFactorials() to printFactorialsExpressions()

--- src/com/startjava/lesson_2_3_4/array/FactorialCalculation.java
+++ src/com/startjava/lesson_2_3_4/array/FactorialCalculation.java
@@ -1,42 +1,50 @@
 package com.startjava.lesson_2_3_4.array;
 
-import java.util.Arrays;
+/* TODO FactorialCalculation
+showFactorials()
+- if (origin.length == 0) System.out.println();
+какой смысл в такой проверке и таком выводе на консоль?
+ С какой дилеммой ты столкнулся, что решил написать такой вывод на консоль?
+
+- число 20 сделай константой. Используй ее везде, где упоминается 20
+*/
 
 public class FactorialCalculation {
+    private static final int LIMIT_NUMBER = 20;
+
     public static void main(String[] args) {
         int[] origin = new int[0];
         long[] factorialsResult = calculate(origin);
-        showFactorials(factorialsResult, origin);
+        printFactorialsExpressions(factorialsResult, origin);
 
         origin = null;
         factorialsResult = calculate(origin);
-        showFactorials(factorialsResult, origin);
+        printFactorialsExpressions(factorialsResult, origin);
 
         origin = new int[]{-5};
         factorialsResult = calculate(origin);
-        showFactorials(factorialsResult, origin);
+        printFactorialsExpressions(factorialsResult, origin);
 
         origin = new int[]{21, 0, 7};
         factorialsResult = calculate(origin);
-        showFactorials(factorialsResult, origin);
+        printFactorialsExpressions(factorialsResult, origin);
 
         origin = new int[]{1, 20, 5, -3};
         factorialsResult = calculate(origin);
-        showFactorials(factorialsResult, origin);
+        printFactorialsExpressions(factorialsResult, origin);
     }
 
     private static long[] calculate(int... origin) {
         long[] factorials = null;
-        if (origin == null) return factorials;
-        if (origin.length == 0) return factorials;
+        if (origin == null || origin.length == 0) return null;
 
         factorials = new long[origin.length];
         for (int i = 0; i < origin.length; i++) {
             long factorial = -1;
             if (origin[i] < 0) {
-                System.out.println("Ошибка: факториал " + origin[i] + " ! не определен");
-            } else if (origin[i] > 20) {
-                System.out.println("Ошибка: факториал " + origin[i] + " ! слишком велик (максимум 20!)");
+                System.out.println("Ошибка: факториал " + origin[i] + "! не определен");
+            } else if (origin[i] > LIMIT_NUMBER) {
+                System.out.println("Ошибка: факториал " + origin[i] + "! слишком велик (максимум 20!)");
             } else {
                 factorial = 1;
                 for (int j = 2; j <= origin[i]; j++) {
@@ -48,30 +56,30 @@ public class FactorialCalculation {
         return factorials;
     }
 
-    private static void showFactorials(long[] factorials, int... origin) {
+    private static void printFactorialsExpressions(long[] factorials, int... origin) {
         if (origin == null) return;
-        if (origin.length == 0) System.out.println();
-        StringBuilder outputResult = new StringBuilder();
+        StringBuilder expression = new StringBuilder();
         int i = 0;
         for (int num : origin) {
             int temp = 1;
-            StringBuilder tempStr = new StringBuilder();
+            String tempStr = "";
             while (temp <= num) {
                 if (num - temp == 0) {
-                    tempStr.append(temp);
+                    tempStr += temp;
                     temp++;
                 } else {
-                    tempStr.append(temp).append(" * ");
+                    tempStr += temp + " * ";
                     temp++;
                 }
             }
-            outputResult.append((num == 0 || num == 1)
+            expression.append((num == 0 || num == 1)
                     ? num + "! = 1" + "\n"
-                    : (num > 0 && num <= 20)
-                    ? num + "! = " + tempStr.append(" = ").append(factorials[i]).append("\n")
+                    : (num > 0 && num <= LIMIT_NUMBER)
+                    ? num + "! = " + tempStr + " = " + factorials[i] + "\n"
                     : "");
             i++;
         }
-        System.out.println(outputResult);
+        System.out.print(expression);
+        expression.setLength(0);
     }
 }
