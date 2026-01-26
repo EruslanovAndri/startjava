e997f44bc8061aeffa9f628cd41d6d5334b88d96
EruslanovAndrei <eruslanov84as@gmail.com>
2026-01-26 12:54:54 +0300

    printArray() method was changed

--- src/com/startjava/lesson_2_3_4/array/ElementRemoverByIndex.java
+++ src/com/startjava/lesson_2_3_4/array/ElementRemoverByIndex.java
@@ -1,75 +1,73 @@
 package com.startjava.lesson_2_3_4.array;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 public class ElementRemoverByIndex {
     public static void main(String[] args) {
-        double[] origin = fillArray();
-        double[] changed = remove(origin, -1);
-        print(origin, changed, -1);
+        float[] originArray = fillArray();
+        float[] changedArray = remove(originArray, -1);
+        printArray(originArray, changedArray, -1);
 
-        origin = fillArray();
-        changed = remove(origin, 15);
-        print(origin, changed, 15);
+        originArray = fillArray();
+        changedArray = remove(originArray, 15);
+        printArray(originArray, changedArray, 15);
 
-        origin = fillArray();
-        changed = remove(origin, 0);
-        print(origin, changed, 0);
+        originArray = fillArray();
+        changedArray = remove(originArray, 0);
+        printArray(originArray, changedArray, 0);
 
-        origin = fillArray();
-        changed = remove(origin, 14);
-        print(origin, changed, 14);
+        originArray = fillArray();
+        changedArray = remove(originArray, 14);
+        printArray(originArray, changedArray, 14);
     }
 
-    private static double[] fillArray() {
+    private static float[] fillArray() {
         Random random = new Random();
-        double[] randomArray = new double[15];
+        float[] randomArray = new float[15];
         for (int i = 0; i < randomArray.length; i++) {
-            randomArray[i] = random.nextDouble(0, 1);
+            randomArray[i] = random.nextFloat();
         }
         return randomArray;
     }
 
-    private static double[] remove(double[] origin, int index) {
-        double[] changed = null;
+    private static float[] remove(float[] originArray, int index) {
         if (index < 0) {
-            System.out.println("Ошибка! Индекс не может быть меньше 0.");
+            System.out.println("Ошибка! Индекс (" + index + ") не может быть меньше 0.");
             return null;
-        } else if (index > origin.length - 1) {
-            System.out.println("Ошибка! Индекс больше длины массива.");
+        }
+        if (index > originArray.length - 1) {
+            System.out.println("Ошибка! Индекс (" + index + ") больше длины массива.");
             return null;
-        } else {
-            double value = origin[index];
-            changed = new double[origin.length];
-            for (int i = 0; i < origin.length; i++) {
-                if (value < origin[i]) {
-                    changed[i] = 0;
-                } else {
-                    changed[i] = origin[i];
-                }
+        }
+
+        float valueByIndex = originArray[index];
+        float[] changedArray = new float[originArray.length];
+        System.arraycopy(originArray, 0, changedArray, 0, originArray.length);
+        for (int i = 0; i < originArray.length; i++) {
+            if (originArray[i] > valueByIndex) {
+                changedArray[i] = 0;
             }
         }
-        return changed;
+        return changedArray;
     }
 
-    private static void print(double[] origin, double[] changed, int index) {
-        if (index >= 0 && index < origin.length) {
-            System.out.println("\nИсходный массив:");
-            for (int i = 0; i < 8; i++) {
-                System.out.printf("%6.3f", origin[i]);
-            }
-            System.out.println();
-            for (int i = 8; i < 15; i++) {
-                System.out.printf("%6.3f", origin[i]);
-            }
-            System.out.printf("%n%s%.3f%n%n", " Значение из ячейки по переданному индексу = ", origin[index]);
-            System.out.println("Измененный массив:");
-            for (int i = 0; i < 8; i++) {
-                System.out.printf("%6.3f", changed[i]);
-            }
-            System.out.println();
-            for (int i = 8; i < 15; i++) {
-                System.out.printf("%6.3f", changed[i]);
+    private static void printArray(float[] originArray, float[] changedArray, int index) {
+        String[] arrayName = {"Исходный массив.", "Измененный массив."};
+        float[][] arrays = {originArray, changedArray};
+        if (index >= 0 && index < originArray.length) {
+            for (int i = 0; i < arrayName.length; i++) {
+                System.out.println("\n" + arrayName[i]);
+                for (int j = 0; j < 8; j++) {
+                    System.out.printf("%6.3f", arrays[i][j]);
+                }
+                System.out.println();
+                for (int j = 8; j < 15; j++) {
+                    System.out.printf("%6.3f", arrays[i][j]);
+                }
+                if (i == 0) {
+                    System.out.printf("%n%s%4.3f%n", "Переданный индекс = ", arrays[i][index]);
+                }
             }
             System.out.println();
         }
