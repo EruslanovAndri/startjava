11b7af30bfbebd5fc71682c5022047221509186b
EruslanovAndrei <eruslanov84as@gmail.com>
2026-02-11 12:55:33 +0300

    add printValidateResult()

--- src/com/startjava/lesson_2_3_4/array/PasswordCracker.java
+++ src/com/startjava/lesson_2_3_4/array/PasswordCracker.java
@@ -8,29 +8,29 @@ public class PasswordCracker {
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String RESET = "\033[0m";
     private static final int MIN_LENGTH = 8;
-    private static char[][] passwordBlackList = {
+    private static final int ROTATION_LIMIT = 12;
+    public static char[][] passwordBlackList = {
             {'a', 'd', 'm', 'i', 'n'},
             {'q', 'w', 'e', 'r', 't', 'y'},
             {'1', '2', '3', '4', '5', '6'}};
 
     public static void main(String[] args) {
         char[] password = {'1', '2', '3', '4', '5', '6'};
-        checkPassword(password);
+        boolean isStrongPassword = checkPassword(password);
+        printValidateResult(isStrongPassword, password);
 
         password = generatePassword();
-        checkPassword(password);
+        isStrongPassword = checkPassword(password);
+        printValidateResult(isStrongPassword, password);
 
         password = generatePassword();
-        checkPassword(password);
+        isStrongPassword = checkPassword(password);
+        printValidateResult(isStrongPassword, password);
 
         password = generatePassword();
-        checkPassword(password);
+        isStrongPassword = checkPassword(password);
+        printValidateResult(isStrongPassword, password);
     }
-
-    public char[][] getPasswordBlackList() {
-        return this.passwordBlackList;
-    }
-
     private static char[] generatePassword() {
         Random random = new Random();
         int length = random.nextInt(6, 13);
@@ -41,41 +41,25 @@ public class PasswordCracker {
         return password;
     }
 
-    private static void runSpinner() {
-        char[] spinner = {'-', '\\', '|', '/'};
-        int counter = 0;
-        StringBuilder spinnerSb = new StringBuilder();
-        for (int i = 0; i < 36; i++) {
-            spinnerSb.append("\b" + spinner[counter % spinner.length]);
-            System.out.print(spinnerSb.append("\r"));
-            counter++;
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private static void checkPassword(char[] password) {
+    private static boolean checkPassword(char[] password) {
+        boolean hasChecked = false;
         runSpinner();
-        PasswordCracker passwordCracker = new PasswordCracker();
-        for (char[] element : passwordCracker.getPasswordBlackList()) {
-            if (Arrays.equals(element, password)) {
+        if (password == null) return false;
+        for (char[] inputPassword : passwordBlackList) {
+            if (Arrays.equals(inputPassword, password)) {
                 System.out.println("Не используйте пароли из списка популярных. - " +
                         Arrays.toString(password));
-                System.out.println(passwordCracker.getClass().getName());
-                return;
+                System.out.println("https://nordpass.com/most-common-passwords-list");
+                return false;
             }
         }
-
         if (password.length == 0) {
             System.out.println("Пароль не может быть пустым - " + Arrays.toString(password));
-            return;
+            return false;
         }
         if (password.length < MIN_LENGTH) {
             System.out.println("Пароль должен быть не менее 8 символов. - " + Arrays.toString(password));
-            return;
+            return false;
         }
 
         boolean hasDigits = false;
@@ -95,24 +79,46 @@ public class PasswordCracker {
             }
         }
 
-        if (password != null && password.length > MIN_LENGTH && hasDigits &&
-                !hasSpecialSymbols && !hasUpperCaseLetters && !hasLowerCaseLetters) {
+        boolean hasLetters = (hasLowerCaseLetters && hasUpperCaseLetters) ? true : false;
+        if (password.length > MIN_LENGTH && hasDigits &&
+                !hasSpecialSymbols && !hasLetters) {
             System.out.println("Пароль содержит только цифры - " + Arrays.toString(password));
-        } else if (!hasDigits && !hasSpecialSymbols && hasUpperCaseLetters && hasLowerCaseLetters) {
+        } else if (!hasDigits && !hasSpecialSymbols && hasLetters) {
             System.out.println("Пароль содержит только буквы - " + Arrays.toString(password));
-        } else if (hasSpecialSymbols && !hasDigits && !hasUpperCaseLetters && !hasLowerCaseLetters) {
-            System.out.println("Пароль содердит только спец. символы - " + Arrays.toString(password));
-        } else if (hasDigits && hasUpperCaseLetters && hasLowerCaseLetters && !hasSpecialSymbols) {
+        } else if (hasSpecialSymbols && !hasDigits && !hasLetters) {
+            System.out.println("Пароль содержит только спец. символы - " + Arrays.toString(password));
+        } else if (hasDigits && hasLetters && !hasSpecialSymbols) {
             System.out.println("Пароль не содержит спец. символы - " + Arrays.toString(password));
-        } else if (hasDigits && hasSpecialSymbols && !hasUpperCaseLetters && !hasLowerCaseLetters) {
+        } else if (hasDigits && hasSpecialSymbols && !hasLetters) {
             System.out.println("Пароль не содержит буквы нижнего и верхнего регистров - " +
                     Arrays.toString(password));
-        } else if (hasDigits && hasSpecialSymbols && hasUpperCaseLetters && hasLowerCaseLetters) {
+        } else if (hasDigits && hasSpecialSymbols && hasLetters) {
+            hasChecked = true;
+        }
+        return hasChecked;
+    }
+
+    private static void printValidateResult(boolean pass, char[] password) {
+        if (pass == true) {
             System.out.println(ANSI_GREEN + "✓ Password cracked: - " + Arrays.toString(password) +
                     RESET);
         } else {
             System.out.println(ANSI_RED + "✗ Strong password: - " + Arrays.toString(password) +
                     RESET);
+        }
+    }
+
+    private static void runSpinner() {
+        char[] spins = {'-', '\\', '|', '/'};
+        StringBuilder spinner = new StringBuilder();
+        for (int i = 0; i < ROTATION_LIMIT; i++) {
+            spinner.append("\b").append(spins[i % spins.length]);
+            System.out.print(spinner.append("\r"));
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
