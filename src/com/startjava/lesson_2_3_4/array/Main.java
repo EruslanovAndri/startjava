9f05fb520ec57b99c0f4faeef8ac42a3876bbfc9
EruslanovAndrei <eruslanov84as@gmail.com>
2026-02-25 12:50:18 +0300

    разместил методы в алфавитном порядке и создал отдельные методы под каждое задание для вызова их в Main

--- src/com/startjava/lesson_2_3_4/array/Main.java
+++ src/com/startjava/lesson_2_3_4/array/Main.java
@@ -1,75 +1,88 @@
 package com.startjava.lesson_2_3_4.array;
 
 public class Main {
+    private static final int LENGTH = 15;
+
     public static void main(String[] args) {
-        char[][] sortedChars = {Arrays.fillArraySortedChars('0', '9', true),
-                Arrays.fillArraySortedChars('/', '!', false),
-                Arrays.fillArraySortedChars('A', 'Z', false)};
-        for (char[] chars : sortedChars) {
-            Console.printTriangle(chars);
-        }
+        reverseBankTransactions();
 
-        System.out.println();
-        int[][] origins = {{}, null, {5}, {6, 8, 9, 1}, {13, 8, 5, 3, 2, 1, 1}};
-        for (int[] origin : origins) {
-            int[] reversedTransaction = Arrays.reverse(origin);
-            Console.printTransactions(origin, reversedTransaction);
-        }
+        calculateFactorial();
 
-        System.out.println();
-        float[] originArray = Arrays.fillRandomArray();
-        float[] changedArray = Arrays.remove(originArray, -1);
-        Console.printArray(originArray, changedArray, -1);
-        originArray = Arrays.fillRandomArray();
-        changedArray = Arrays.remove(originArray, 15);
-        Console.printArray(originArray, changedArray, 15);
-        originArray = Arrays.fillRandomArray();
-        changedArray = Arrays.remove(originArray, 0);
-        Console.printArray(originArray, changedArray, 0);
-        originArray = Arrays.fillRandomArray();
-        changedArray = Arrays.remove(originArray, 14);
-        Console.printArray(originArray, changedArray, 14);
+        deleteElementByIndex(LENGTH, -1);
+        deleteElementByIndex(LENGTH, 15);
+        deleteElementByIndex(LENGTH, 0);
+        deleteElementByIndex(LENGTH, 14);
 
-        System.out.println();
+        fillSortedChars('0', '9', true);
+        fillSortedChars('/', '!', false);
+        fillSortedChars('A', 'Z', false);
+
+        fillSortedUniqueNumbers(-50, -10, 23);
+        fillSortedUniqueNumbers(10, 50, 10);
+        fillSortedUniqueNumbers(-34, -34, 1);
+        fillSortedUniqueNumbers(-1, 2, -3);
+        fillSortedUniqueNumbers(5, -8, 2);
+
+        checkPasswordStrength();
+
+        imitateTypewriter();
+    }
+
+    private static void calculateFactorial() {
         int[][] factorialOrigins;
         factorialOrigins = new int[][]{{0}, null, {-5}, {21, 0, 7}, {1, 20, 5, -3}};
         for (int[] origin : factorialOrigins) {
             long[] factorialResult = Arrays.calculateFactorial(origin);
             Console.printFactorialsExpressions(factorialResult, origin);
         }
+    }
 
-        System.out.println();
-        char[][] password = {{'1', '2', '3', '4', '5', '6'}, Arrays.generatePassword(),
-                Arrays.generatePassword(), Arrays.generatePassword()};
+    private static void checkPasswordStrength() {
+        char[][] password = {{'1', '2', '3', '4', '5', '6'}, Arrays.generateCharArrayInRange(),
+                Arrays.generateCharArrayInRange(), Arrays.generateCharArrayInRange()};
         for (int i = 0; i < password.length; i++) {
             boolean isChecked = Arrays.checkPassword(password[i]);
-            Console.printValidateResult(isChecked, password[i]);
+            Console.printValidateResult("x Strong Password: - ",
+                    "✓ Password cracked: - ", isChecked, password[i]);
         }
+    }
 
-        System.out.println();
+    private static void deleteElementByIndex(int length, int index) {
+        float[] inputRandom = Arrays.fillRandomArray(length);
+        float[] clean = Arrays.removeBiggerElement(inputRandom, index);
+        Console.printArray(inputRandom, clean, index);
+    }
+
+    private static void fillSortedChars(char leftSide, char rightSide, boolean direction) {
+        char[] sortedChars = Arrays.fillArraySortedChars(leftSide, rightSide, direction);
+        Console.printTriangle(sortedChars);
+    }
+
+    private static void fillSortedUniqueNumbers(int start, int end, int limitNumbers) {
+        int[] unique = Arrays.fillSortedUniqueNumbersArray(start, end, limitNumbers);
+        Console.printInLimitNumbersRange(unique, limitNumbers);
+    }
+
+    private static void imitateTypewriter() {
         String[] texts = {
                 "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
                         "- James Gosling",
                 "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
                         "- Robert Martin",
                 null, ""};
-
         for (String text : texts) {
             int[] shortAndLongWord = Arrays.findShortestLongestWordIndex(text);
             StringBuilder convertToUpperCase = Arrays.convertToUpperCase(shortAndLongWord, text);
             Console.imitateTypewriter(convertToUpperCase);
         }
+    }
 
-        System.out.println();
-        int[] uniqueNumbers = Arrays.fillSortedUniqueNumbersArray(-50, -10, 23);
-        Console.printInLimitNumbersRange(uniqueNumbers, 23);
-        uniqueNumbers = Arrays.fillSortedUniqueNumbersArray(10, 50, 10);
-        Console.printInLimitNumbersRange(uniqueNumbers, 10);
-        uniqueNumbers = Arrays.fillSortedUniqueNumbersArray(-34, -34, 1);
-        Console.printInLimitNumbersRange(uniqueNumbers, 1);
-        uniqueNumbers = Arrays.fillSortedUniqueNumbersArray(-1, 2, -3);
-        Console.printInLimitNumbersRange(uniqueNumbers, -3);
-        uniqueNumbers = Arrays.fillSortedUniqueNumbersArray(5, -8, 2);
-        Console.printInLimitNumbersRange(uniqueNumbers, 2);
+    private static void reverseBankTransactions() {
+        int[][] inputTransactions = {{}, null, {5}, {6, 8, 9, 1}, {13, 8, 5, 3, 2, 1, 1}};
+        for (int[] transaction : inputTransactions) {
+            int[] reversed = Arrays.reverse(transaction);
+            Console.print("Исходные транзакции:", transaction,
+                    "В обратном порядке:", reversed);
+        }
     }
 }
