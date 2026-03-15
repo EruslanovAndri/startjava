77b9db402f3894e70b6d595447e516417687dd22
EruslanovAndrei <eruslanov84as@gmail.com>
2026-03-15 12:20:46 +0300

    разделил метод startGame

--- src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
+++ src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
@@ -4,11 +4,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HangmanGame {
-    public static int counter;
+    public static int totalSteps;
     public static int attempts;
-    public static int guessedLetters;
+    public static int guessedLettersCounter;
+    public static StringBuilder wrongLetters = new StringBuilder();
+    public static StringBuilder rightLetters = new StringBuilder();
     public static Scanner scanner = new Scanner(System.in);
-    public static String[] hangman = {
+    public static String[] gallows = {
             "______",
             "|    |",
             "|    @",
@@ -17,94 +19,105 @@ public class HangmanGame {
             "| GAME OVER!"
     };
 
-    public static void startGame() {
+    public void startGame() {
         String randomWord = getRandomWord();
-        StringBuilder mistakes = new StringBuilder();
-        StringBuilder usedLetters = new StringBuilder();
         StringBuilder hideWord = new StringBuilder(randomWord.replaceAll("[а-я-ё]", "*"));
-
         printMessage("Игра началась!\nУгадайте слово загаданное компьютером (" + hideWord + ")");
 
-        while (guessedLetters != randomWord.length()) {
-            char inputChar = getChar();
-            attempts++;
-
-            if (!randomWord.contains(Character.toString(inputChar))) {
-                printMessage("Буквы (" + inputChar + ") нет в слове (" + hideWord + ")");
-                if (!mistakes.toString().contains(Character.toString(inputChar))) {
-                    mistakes.append(inputChar).append(" ");
-                    counter++;
+        while (guessedLettersCounter != randomWord.length()) {
+            char letter = inputLetter();
+            if (!randomWord.contains(Character.toString(letter))) {
+                printMessage("Буквы (" + letter + ") нет в слове (" + hideWord + ")");
+                if (addLetter(letter, wrongLetters)) {
+                    addTotalStep();
                 }
-                drawHangman();
-            } else if (randomWord.contains(Character.toString(inputChar))) {
-                for (int i = 0; i < randomWord.length(); i++) {
-                    if (randomWord.charAt(i) == inputChar) {
-                        hideWord.replace(i, i + 1, Character.toString(inputChar).toUpperCase());
-                    }
+            } else if (randomWord.contains(Character.toString(letter))) {
+                printMessage("Буква (" + letter + ") есть в слове (" + hideWord + ")");
+                replaceAsterisk(randomWord, letter, hideWord);
+                if (addLetter(letter, rightLetters)) {
+                    guessedLettersCounter++;
+                    subtractTotalStep();
                 }
-                if (!usedLetters.toString().contains(Character.toString(inputChar))) {
-                    usedLetters.append(inputChar);
-                    guessedLetters++;
-                    if (counter != 0) {
-                        counter--;
-                        drawHangman();
-                    }
-                }
-                printMessage("Буква (" + inputChar + ") есть в слове (" + hideWord + ")");
             }
-            printMessage("Ошибочные буквы - " + mistakes);
-            printMessage("Правильные буквы - " + guessedLetters);
+            attempts++;
+            drawGallows();
+            printMessage("Ошибочные буквы - " + wrongLetters);
+            printMessage("Количество правильных букв - " + guessedLettersCounter);
+            printMessage("Правильные буквы - " + rightLetters);
 
             if (randomWord.equals(hideWord.toString().toLowerCase())) {
                 printMessage("Вы угадали слово (" + randomWord.toUpperCase() +
                         ") за (" + attempts + ") попыток.");
-                counter = 0;
-                usedLetters.setLength(0);
-                attempts = 0;
-                guessedLetters = 0;
+                resetCounters();
                 break;
-            } else if (counter == hangman.length) {
+            } else if (totalSteps == gallows.length) {
                 printMessage("Вы не угадали слово (" + randomWord.toUpperCase() + ")");
-                counter = 0;
-                usedLetters.setLength(0);
-                attempts = 0;
-                guessedLetters = 0;
+                resetCounters();
                 break;
             }
         }
     }
 
-    private static void drawHangman() {
-        for (int i = 0; i < counter; i++) {
-            System.out.println(hangman[i]);
+    private static void drawGallows() {
+        for (int i = 0; i < totalSteps; i++) {
+            System.out.println(gallows[i]);
         }
     }
 
-    private static char getChar() {
-        char inputChar = '-';
+    private static char inputLetter() {
         boolean isCyrillic = true;
         while (isCyrillic) {
             System.out.print("Введите букву от а-я: ");
-            char temp = scanner.next().toLowerCase().charAt(0);
-            if (temp >= 'а' && temp <= 'я' || temp == 'ё') {
-                inputChar = temp;
-                isCyrillic = false;
+            char letter = scanner.next().toLowerCase().charAt(0);
+            if (letter >= 'а' && letter <= 'я' || letter == 'ё') {
+                return letter;
             } else {
                 printMessage("Некорректный ввод!");
             }
         }
-        return inputChar;
+        return ' ';
     }
 
     private static String getRandomWord() {
         String[] words = {"ребёнок", "шалаш", "земля", "рыба", "мир"};
         Random random = new Random();
         int index = random.nextInt(words.length);
-        String secretWord = words[index].toLowerCase();
-        return secretWord;
+        return words[index].toLowerCase();
     }
 
     private static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    private static boolean addLetter(char letter, StringBuilder letters) {
+        if (!letters.toString().contains(Character.toString(letter))) {
+            letters.append(letter).append(" ");
+            return true;
+        }
+        return false;
+    }
+
+    private static void addTotalStep() {
+        totalSteps++;
+    }
+
+    private static void subtractTotalStep() {
+        if (totalSteps != 0) totalSteps--;
+    }
+
+    private static void resetCounters() {
+        totalSteps = 0;
+        attempts = 0;
+        guessedLettersCounter = 0;
+        wrongLetters.setLength(0);
+        rightLetters.setLength(0);
+    }
+
+    private static void replaceAsterisk(String originWord, char letter, StringBuilder targetWord) {
+        for (int i = 0; i < originWord.length(); i++) {
+            if (originWord.charAt(i) == letter) {
+                targetWord.replace(i, i + 1, Character.toString(letter).toUpperCase());
+            }
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/hangman/HangmanGameMain.java
+++ src/com/startjava/lesson_2_3_4/hangman/HangmanGameMain.java
@@ -1,11 +1,15 @@
 package com.startjava.lesson_2_3_4.hangman;
 
-public class HangmanGameMain extends HangmanGame {
+import java.util.Scanner;
+
+public class HangmanGameMain {
 
     public static void main(String[] args) {
+        HangmanGame game = new HangmanGame();
         String again = "yes";
+        Scanner scanner = new Scanner(System.in);
         while ("yes".equals(again)) {
-            startGame();
+            game.startGame();
             do {
                 System.out.print("Хотите продолжить игру [yes/no]? ");
                 again = scanner.next().toLowerCase();
