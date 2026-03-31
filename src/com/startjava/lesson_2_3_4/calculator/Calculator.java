b3c493c3a6f9e15c2c72bd7d7e8682611cbb7dd6
EruslanovAndrei <eruslanov84as@gmail.com>
2026-03-31 16:25:27 +0300

    удалил поле result и изменил возврат результата в каждой ветке switch case

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -5,7 +5,6 @@ public class Calculator {
     private int number1;
     private String sing;
     private int number2;
-    private double result;
 
     public double calculate(String input) {
         String[] expression = input.split(" ");
@@ -14,23 +13,23 @@ public class Calculator {
         number2 = Integer.parseInt(expression[2]);
         if (number2 == 0 && (sing.equals("/") || sing.equals("%"))) {
             System.out.println("Деление на ноль запрещено.");
-            return result = Double.NaN;
+            return Double.NaN;
         }
         switch (sing) {
             case "+":
-                return result = (double) number1 + number2;
+                return number1 + number2;
             case "-":
-                return result = (double) number1 - number2;
+                return number1 - number2;
             case "*":
-                return result = (double) number1 * number2;
+                return number1 * number2;
             case "/":
-                return result = (double) number1 / number2;
+                return (double) number1 / number2;
             case "^":
-                return result = Math.pow(number1, number2);
+                return Math.pow(number1, number2);
             case "%":
-                return result = Math.floorMod(number1, number2);
+                return Math.floorMod(number1, number2);
             default:
         }
-        return result;
+        return 0.0;
     }
 }
