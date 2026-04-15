b721a2c5b80a96d712fdf684b019b8d68134839d
EruslanovAndrei <eruslanov84as@gmail.com>
2026-04-15 11:09:07 +0300

    add empty constractor

--- src/com/startjava/lesson_2_3_4/calculator/InvalidSingException.java
+++ src/com/startjava/lesson_2_3_4/calculator/InvalidSingException.java
@@ -1,6 +1,10 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+
+
 public class InvalidSingException extends RuntimeException {
+    public InvalidSingException() {
+    }
     public InvalidSingException(String message) {
         super(message);
     }
