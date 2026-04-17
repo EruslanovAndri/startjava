662a8c9db9fd63e794ac11dc5b202583a0d444e4
EruslanovAndrei <eruslanov84as@gmail.com>
2026-04-17 16:53:29 +0300

    сократил блок try/catch, переименовал exception class name

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,40 +1,43 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import com.startjava.lesson_2_3_4.guess.NumberOutOfRangeException;
+
 import java.util.Arrays;
 
 public class Calculator {
-    private static final int MAX_LENGTH = 3;
+    private static final int EXPRESSION_LENGTH = 3;
 
     public static double calculate(String expression) {
         String[] args = expression.split(" ");
-        double result = 0;
+        int number1;
+        String sign;
+        int number2;
+        throwIfInvalidExpressionLength(args);
         try {
-            validateExpressionLength(args);
-            int number1 = Integer.parseInt(args[0]);
-            String sign = args[1];
-            int number2 = Integer.parseInt(args[2]);
-            if (number2 == 0 && (sign.equals("/") || sign.equals("%"))) {
-                throw new ArithmeticException("Деление на ноль запрещено.");
-            }
-            result = switch (sign) {
-                case "+" -> number1 + number2;
-                case "-" -> number1 - number2;
-                case "*" -> number1 * number2;
-                case "^" -> Math.pow(number1, number2);
-                case "/" -> (double) number1 / number2;
-                case "%" -> Math.floorMod(number1, number2);
-                default -> throw new UnsupportedSignException("Ошибка: операция '" + sign +
-                        "' не поддерживается.\nДопустимые операции - [+-/*^%]");
-            };
+            number1 = Integer.parseInt(args[0]);
+            sign = args[1];
+            number2 = Integer.parseInt(args[2]);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("Необходимо ввести только цифры");
+            throw new NumberOutOfRangeException("Необходимо ввести только целые цифры");
         }
-        return result;
+        if (number2 == 0 && (sign.equals("/") || sign.equals("%"))) {
+            throw new ArithmeticException("Деление на ноль запрещено.");
+        }
+        return switch (sign) {
+            case "+" -> number1 + number2;
+            case "-" -> number1 - number2;
+            case "*" -> number1 * number2;
+            case "^" -> Math.pow(number1, number2);
+            case "/" -> (double) number1 / number2;
+            case "%" -> Math.floorMod(number1, number2);
+            default -> throw new UnsupportedSignException("Ошибка: операция '" + sign +
+                    "' не поддерживается.\nДопустимые операции - [+-/*^%]");
+        };
     }
 
-    private static void validateExpressionLength(String[] args) {
-        if (args.length != MAX_LENGTH) {
-            throw new IncorrectExpressionException("Введённое выражение '" + Arrays.toString(args) +
+    private static void throwIfInvalidExpressionLength(String[] args) {
+        if (args.length != EXPRESSION_LENGTH) {
+            throw new ExpressionLengthException("Введённое выражение '" + Arrays.toString(args) +
                     "' не соответствует необходимому формату.\nПрограмма ожидает: 2 + 2");
         }
     }
--- src/com/startjava/lesson_2_3_4/calculator/IncorrectExpressionException.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lesson_2_3_4.calculator;
-
-public class IncorrectExpressionException extends RuntimeException {
-    public IncorrectExpressionException(final String message) {
-        super(message);
-    }
-}
