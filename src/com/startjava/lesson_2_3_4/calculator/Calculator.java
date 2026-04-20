88ec8885618b7e87832a7a6a4011ef94fc3c950c
EruslanovAndrei <eruslanov84as@gmail.com>
2026-04-20 12:36:26 +0300

    добавил метод divideNumber и реализовал в нем проверку деление на ноль

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,7 +1,6 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import com.startjava.lesson_2_3_4.guess.NumberOutOfRangeException;
-
 import java.util.Arrays;
 
 public class Calculator {
@@ -10,26 +9,21 @@ public class Calculator {
     public static double calculate(String expression) {
         String[] args = expression.split(" ");
         int number1;
-        String sign;
+        String sign = args[1];;
         int number2;
         throwIfInvalidExpressionLength(args);
         try {
             number1 = Integer.parseInt(args[0]);
-            sign = args[1];
             number2 = Integer.parseInt(args[2]);
         } catch (NumberFormatException e) {
-            throw new NumberOutOfRangeException("Необходимо ввести только целые цифры");
-        }
-        if (number2 == 0 && (sign.equals("/") || sign.equals("%"))) {
-            throw new ArithmeticException("Деление на ноль запрещено.");
+            throw new NumberOutOfRangeException("Необходимо ввести только целые числа.");
         }
         return switch (sign) {
             case "+" -> number1 + number2;
             case "-" -> number1 - number2;
             case "*" -> number1 * number2;
             case "^" -> Math.pow(number1, number2);
-            case "/" -> (double) number1 / number2;
-            case "%" -> Math.floorMod(number1, number2);
+            case "/", "%" -> divideNumber(number1, number2, sign);
             default -> throw new UnsupportedSignException("Ошибка: операция '" + sign +
                     "' не поддерживается.\nДопустимые операции - [+-/*^%]");
         };
@@ -40,5 +34,13 @@ public class Calculator {
             throw new ExpressionLengthException("Введённое выражение '" + Arrays.toString(args) +
                     "' не соответствует необходимому формату.\nПрограмма ожидает: 2 + 2");
         }
+    }
+
+    private static double divideNumber(int number1, int number2, String sign) {
+        if (number2 == 0 && (sign.equals("/") || sign.equals("%"))) {
+            throw new ArithmeticException("Деление на ноль запрещено.");
+        }
+        return (sign.equals("/")) ? (double) number1 / number2 :
+                Math.floorMod(number1, number2);
     }
 }
