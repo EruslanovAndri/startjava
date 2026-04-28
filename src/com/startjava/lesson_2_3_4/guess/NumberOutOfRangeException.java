de4c5c52abfd9593abe9c66280897c69e1b4768a
EruslanovAndrei <eruslanov84as@gmail.com>
2026-04-28 18:21:18 +0300

    удалил лишние конструкторы

--- src/com/startjava/lesson_2_3_4/guess/NumberOutOfRangeException.java
+++ src/com/startjava/lesson_2_3_4/guess/NumberOutOfRangeException.java
@@ -1,22 +1,7 @@
 package com.startjava.lesson_2_3_4.guess;
 
-public class NumberOutOfRangeException extends RuntimeException{
-    public NumberOutOfRangeException() {
-    }
-
+public class NumberOutOfRangeException extends RuntimeException {
     public NumberOutOfRangeException(final String message) {
         super(message);
-    }
-
-    public NumberOutOfRangeException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-
-    public NumberOutOfRangeException(final Throwable cause) {
-        super(cause);
-    }
-
-    public NumberOutOfRangeException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
