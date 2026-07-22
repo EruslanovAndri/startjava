2e9cb5c6f1d7951c96ce9b9401579a0ebfee94ca
EruslanovAndrei <eruslanov84as@gmail.com>
2026-07-22 14:32:31 +0300

    remane IndexOutOfBoundsMenuException

--- src/com/startjava/lesson_2_3_4/bookcase/exception/IndexOfBoundsMenuException.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lesson_2_3_4.bookcase.exception;
-
-public class IndexOfBoundsMenuException extends RuntimeException {
-    public IndexOfBoundsMenuException(String message) {
-        super(message);
-    }
-}
---
+++ src/com/startjava/lesson_2_3_4/bookcase/exception/IndexOutOfBoundsMenuException.java
@@ -0,0 +1,7 @@
+package com.startjava.lesson_2_3_4.bookcase.exception;
+
+public class IndexOutOfBoundsMenuException extends RuntimeException {
+    public IndexOutOfBoundsMenuException(String message) {
+        super(message);
+    }
+}
