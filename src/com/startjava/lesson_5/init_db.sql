597e9db0232c87455085039ad3e66d7532bad499
EruslanovAndrei <eruslanov84as@gmail.com>
2026-08-07 22:50:07 +0300

    отредактировал написание запросов по правилам

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -4,16 +4,17 @@ CREATE DATABASE robots;
 
 DROP TABLE IF EXISTS jaegers;
 
-CREATE TABLE jaegers(
-	id SERIAL 	PRIMARY KEY,
-	model_name 	VARCHAR (20),
+CREATE TABLE jaegers (
+    id SERIAL 	PRIMARY KEY,
+	model_name 	VARCHAR(20),
 	mark		INT,
 	height		REAL,
-	weight		NUMERIC,
-	status		VARCHAR (16),
-	origin		VARCHAR (30),
+	weight		REAL,
+	status		VARCHAR(16),
+	origin		VARCHAR(30),
 	launch		DATE,
-	kaiju_kill	INT);
+	kaiju_kill	INT
+);
 	
 
 \ir 'populate.sql'
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -6,7 +6,7 @@ SELECT *
 \echo Вывод не уничтоженных роботов
 SELECT * 
     FROM jaegers
-   WHERE status = 'Destroyed';
+   WHERE status != 'Destroyed';
 
 \echo Вывод только роботов серии Mark-1 и Mark-4
 SELECT * 
@@ -15,30 +15,30 @@ SELECT *
 
 \echo Вывод всех роботов, кроме Mark-1 и Mark-4, отсортировав таблицу по убыванию по столбцу mark
 SELECT * 
-    FROM jaegers 
-   WHERE mark NOT IN(1,4) 
+    FROM jaegers
+   WHERE mark NOT IN(1,4)
    ORDER BY mark DESC;
 
 \echo Вывод информацию о самых старых роботах
 SELECT * 
-    FROM jaegers 
+    FROM jaegers
    WHERE launch <= (SELECT MIN(launch)
-                        FROM jaegers);
+    FROM jaegers);
 
 \echo Вывод информации из столбцов model_name, mark, launch, kaiju_kill тех роботов, которые уничтожили больше всех kaiju
 SELECT model_name, mark, launch, kaiju_kill
     FROM jaegers
    WHERE kaiju_kill = (SELECT MAX(kaiju_kill)
-                           FROM jaegers);
+    FROM jaegers);
 
 \echo Вывод среднего веса роботов, округлив его до трех знаков после запятой. Отображайте над результатом avg_weight вместо round
-SELECT ROUND(AVG(weight),3) AS avg_weight
+SELECT ROUND(AVG(weight)::NUMERIC, 3) AS avg_weight
     FROM jaegers;
 
 \echo увеличьте на единицу количество уничтоженных kaiju у неразрушенных роботов, а затем выведите таблицу
 UPDATE jaegers
     SET kaiju_kill = kaiju_kill + 1
-   WHERE status != 'Destroyed';
+  WHERE status != 'Destroyed';
 
 \echo удалите уничтоженных роботов, а затем выведите оставшихся
 DELETE
